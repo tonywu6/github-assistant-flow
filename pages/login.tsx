cfe5386d116819028295838368309df9c10d5370
Tony Wu <tonywu6@protonmail.com>
2023-06-06 09:51:23 +0800

    fix swr

--- pages/login.tsx
+++ pages/login.tsx
@@ -3,14 +3,20 @@ import useSWR from 'swr'
 
 const AUTH_ENDPOINT = new URL(process.env.GITHUB_OAUTH_ENDPOINT as string)
 
+function createRequest(code: string) {
+  const params = new URLSearchParams({ code })
+  const endpoint = new URL(AUTH_ENDPOINT.href)
+  endpoint.search = params.toString()
+  return () => fetch(endpoint).then((r) => r.json())
+}
+
 export default function Login() {
   const {
     query: { code },
   } = useRouter()
-  const params = new URLSearchParams({ code: String(code) })
-  AUTH_ENDPOINT.search = params.toString()
   const { data, isLoading } = useSWR<{ token?: string }>(
-    AUTH_ENDPOINT.toString(),
+    code,
+    createRequest(code as string),
   )
   if (isLoading) {
     return <div>Loading...</div>
