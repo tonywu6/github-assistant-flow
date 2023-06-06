76087a844a8798aeba220fc6c2cdf438cfe12437
Tony Wu <tonywu6@protonmail.com>
2023-06-06 16:05:09 +0800

    add workflow trigger

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,17 @@
 import useSWRImmutable from 'swr/immutable'
 import { useOctokit } from './login'
+import useSWRMutation from 'swr/mutation'
 
-import { useState } from 'react'
 import styled from 'styled-components'
-import { Image, Input, Button, Form } from 'antd'
+import { Image, Input, Button, Form, Typography } from 'antd'
+import { useState } from 'react'
+
+const HStack = styled.div`
+  display: flex;
+  flex-flow: row nowrap;
+  align-items: baseline;
+  gap: 8px;
+`
 
 function Profile() {
   const { octokit, cacheKey } = useOctokit()
@@ -12,23 +20,24 @@ function Profile() {
     return null
   }
   return (
-    <Image
-      style={{ width: '64px', height: '64px' }}
-      width={64}
-      height={64}
-      src={`${data.data.avatar_url}&s=128`}
-      alt="user profile"
-      placeholder={
-        <Image
-          style={{ width: '64px', height: '64px' }}
-          preview={false}
-          width={64}
-          height={64}
-          alt="user profile"
-          src="data:image/svg+xml;base64,PHN2ZyB3aWR0aD0iNjQiIGhlaWdodD0iNjQiIHhtbG5zPSJodHRwOi8vd3d3LnczLm9yZy8yMDAwL3N2ZyI+PHJlY3Qgd2lkdGg9IjY0IiBoZWlnaHQ9IjY0IiB4PSIwIiB5PSIwIiBmaWxsPSIjZDNkM2QzIiAvPjwvc3ZnPg=="
-        />
-      }
-    />
+    <div style={{ width: '64px', height: '64px' }}>
+      <Image
+        width={64}
+        height={64}
+        src={`${data.data.avatar_url}&s=128`}
+        alt="user profile"
+        placeholder={
+          <Image
+            style={{ width: '64px', height: '64px' }}
+            preview={false}
+            width={64}
+            height={64}
+            alt="user profile"
+            src="data:image/svg+xml;base64,PHN2ZyB3aWR0aD0iNjQiIGhlaWdodD0iNjQiIHhtbG5zPSJodHRwOi8vd3d3LnczLm9yZy8yMDAwL3N2ZyI+PHJlY3Qgd2lkdGg9IjY0IiBoZWlnaHQ9IjY0IiB4PSIwIiB5PSIwIiBmaWxsPSIjZDNkM2QzIiAvPjwvc3ZnPg=="
+          />
+        }
+      />
+    </div>
   )
 }
 
@@ -49,12 +58,18 @@ function LoginButton() {
 }
 
 function CommentSection() {
-  const { octokit, authenticated } = useOctokit()
-  const [submitted, setSubmitted] = useState<string>()
+  const { octokit, cacheKey } = useOctokit()
 
-  if (!authenticated) {
-    return null
-  }
+  const { trigger, data, error } = useSWRMutation(
+    cacheKey('comment'),
+    (url, { arg: { comment } }: { arg: { comment: string } }) =>
+      octokit?.rest.issues.createComment({
+        owner: 'tonywu6',
+        repo: 'github-assistant-flow',
+        issue_number: 1,
+        body: comment,
+      })
+  )
 
   return (
     <Form<{ comment?: string }>
@@ -62,26 +77,64 @@ function CommentSection() {
         if (!comment) {
           return
         }
-        return octokit?.rest.issues
-          .createComment({ owner: 'tonywu6', repo: 'github-assistant-flow', issue_number: 1, body: comment })
-          .then(({ data }) => setSubmitted(data.html_url))
-          .catch(console.error)
+        return trigger({ comment }).catch(console.error)
       }}
       style={{ width: '100%' }}
     >
       <Form.Item name="comment" rules={[{ required: true }]}>
         <Input.TextArea autoSize={{ minRows: 3 }} />
       </Form.Item>
-      {submitted ? (
-        <p>
-          Thank you for participating! See your comment at <a href={submitted}>{submitted}</a>
-        </p>
-      ) : (
-        <Button type="primary" htmlType="submit">
-          Submit comment
-        </Button>
-      )}
+      <HStack>
+        {data?.data.html_url ? (
+          <Typography.Paragraph>
+            Thank you for participating! See your comment at <a href={data?.data.html_url}>{data?.data.html_url}</a>
+          </Typography.Paragraph>
+        ) : (
+          <Button type="primary" htmlType="submit">
+            Submit comment
+          </Button>
+        )}
+        {error ? <Typography.Paragraph type="danger">{String(error)}</Typography.Paragraph> : null}
+      </HStack>
     </Form>
+  )
+}
+
+function WorkflowControl() {
+  const { octokit, cacheKey } = useOctokit()
+  const [clicked, setClicked] = useState(false)
+
+  const { trigger, error } = useSWRMutation(cacheKey('workflow'), () =>
+    octokit?.rest.actions.createWorkflowDispatch({
+      owner: 'tonywu6',
+      repo: 'github-assistant-flow',
+      workflow_id: 'echo.yml',
+      ref: 'main',
+    })
+  )
+
+  return (
+    <HStack>
+      <Button
+        danger
+        onClick={() =>
+          trigger()
+            .then(() => setClicked(true))
+            .catch(console.error)
+        }
+      >
+        Trigger workflow
+      </Button>
+      {error ? <Typography.Paragraph type="danger">{String(error)}</Typography.Paragraph> : null}
+      {clicked ? (
+        <Typography.Paragraph>
+          See workflow runs at{' '}
+          <a href="https://github.com/tonywu6/github-assistant-flow/actions/workflows/echo.yml">
+            https://github.com/tonywu6/github-assistant-flow/actions/workflows/echo.yml
+          </a>
+        </Typography.Paragraph>
+      ) : null}
+    </HStack>
   )
 }
 
@@ -94,6 +147,7 @@ function App() {
     <>
       <Profile />
       <CommentSection />
+      <WorkflowControl />
     </>
   )
 }
