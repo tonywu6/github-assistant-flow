ea2b2ef498b9e1393b51ed3693ac6c749483be27
Tony Wu <tonywu6@protonmail.com>
2023-06-06 09:41:07 +0800

    add login page

---
+++ pages/login.tsx
@@ -0,0 +1,17 @@
+import { useRouter } from 'next/router'
+import useSWR from 'swr'
+
+const AUTH_ENDPOINT = new URL(process.env.GITHUB_OAUTH_ENDPOINT as string)
+
+export default function Login() {
+  const {
+    query: { code },
+  } = useRouter()
+  const params = new URLSearchParams({ code: String(code) })
+  AUTH_ENDPOINT.search = params.toString()
+  const { data, isLoading } = useSWR<{ token?: string }>(AUTH_ENDPOINT)
+  if (isLoading) {
+    return <div>Loading...</div>
+  }
+  return <div>{data?.token}</div>
+}
